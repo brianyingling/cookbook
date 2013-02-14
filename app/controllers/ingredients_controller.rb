8311c839098605e19c54c3d60118a8fc7e3cfa7d
Brian Yingling <brian_yingling@yahoo.com>
2013-02-14 17:47:46 -0500

    finished functionality

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -6,13 +6,22 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.new
   end
   def create
+    Ingredient.create(params[:ingredient])
+    redirect_to ingredients_path
   end
   def update
+    ingredient = Ingredient.find(params[:id])
+    ingredient.update_attributes(params[:ingredient])
+    redirect_to ingredients_path
   end
   def edit
+    @ingredient = Ingredient.find(params[:id])
   end
   def show
+    @ingredient = Ingredient.find(params[:id])
   end
   def destroy
+    Ingredient.delete(params[:id])
+    redirect_to ingredients_path
   end
 end
